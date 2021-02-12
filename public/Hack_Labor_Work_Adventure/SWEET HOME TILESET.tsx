c8fb69db1f705c0eb265278dcb86ee88cd9addc8
Dave Aitken <david.p.aitken@gmail.com>
2021-02-12 15:16:38 +0000

    added new map folder

---
+++ public/Hack_Labor_Work_Adventure/SWEET HOME TILESET.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="SWEET HOME TILESET" tilewidth="32" tileheight="32" tilecount="204" columns="17">
+ <image source="../../Sweet Home Pixel Tile Set/SWEET HOME TILESET.png" width="560" height="384"/>
+</tileset>
---
+++ public/Hack_Labor_Work_Adventure/Scifi Ship-Trans.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Scifi Ship-Trans" tilewidth="16" tileheight="16" tilecount="270" columns="15">
+ <image source="../../SCi-fi Spaceship/Scifi Ship-Trans.png" width="240" height="288"/>
+</tileset>
