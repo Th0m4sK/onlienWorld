72956f3f4a37798efa2beee311dc91748ae5981c
David Aitken <david.p.aitken@gmail.com>
2021-03-01 09:18:19 +0000

    Auto stash before merge of "master" and "origin/master"

--- public/Hack_Labor_Work_Adventure/Moe Assets 001.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Moe Assets 001" tilewidth="32" tileheight="32" tilecount="63" columns="7">
- <image source="../../SCi-fi Spaceship/Scifi Ship.png" width="240" height="288"/>
-</tileset>
